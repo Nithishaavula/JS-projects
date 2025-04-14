96d31aa30d29e15f6ab3d3df9bdf39ea76d15da3
Nithishaavula <nithishaavula12101999@gmail.com>
2025-04-14 13:45:06 +0530

    js

---
+++ main.js
@@ -0,0 +1,6 @@
+let login=document.getElementById("login");
+let password=document.getElementById("password");
+let signup=document.getElementById("signup");
+let forgot=document.getElementById("forgot");
+let submit=document.getElementById("continue");
+console.log(login.value);
